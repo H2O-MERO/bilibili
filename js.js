6d2f2f2a2e663a275cb2ee30ddff0939878b7d95
H2O_MERO <60950002+H2O-MERO@users.noreply.github.com>
2021-01-10 10:48:59 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,17 @@
+const images = document.querySelectorAll('header > div > img')
+
+document.querySelector('header').addEventListener('mousemove' , (e) => {
+    let percentage = e.clientX / window.outerWidth
+    let offset = 10 * percentage
+    let blur = 20
+
+    for( let [index , image] of images.entries()) {
+        offset *= 1.3
+
+        let blurvalue = ( Math.pow((index / images.length - percentage) , 2 ) * blur )
+
+        image.style.setProperty('--offset' , `${offset}px`)
+        image.style.setProperty('--blur' , `${blurvalue}px`)
+
+    }
+})
